aa64325443c8fffe2557a7cc459aba67c1f04011
Marcelo Frias <frias.marcelo@gmail.com>
2021-02-22 18:34:10 -0300

    modal de admin e inicio de java

---
+++ js/admin.js
@@ -0,0 +1,9 @@
+import { Funko } from "./funkoClass.js";
+
+let nuevoFunkopop = new Funko(1, "asdfsdfs", "adadf23", "dc", "adfafas", "adfa")
+console.log(nuevoFunkopop)
+
+window.agregarFunkopop = function(event) {
+    event.preventDefault();
+    console.log("dentro de agregar funko");
+}
